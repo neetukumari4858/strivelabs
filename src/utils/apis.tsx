26307ea85bf13abb1a60b4fb8d51c0dd57830314
neetukumari4858 <neetu21@navgurukul.org>
2024-11-13 15:10:23 +0530

    packege changes for Typescript

--- src/utils/apis.tsx
+++ src/utils/apis.tsx
@@ -1,5 +1,4 @@
 export const apiUrl='https://api.countrylayer.com/v2'
-// export const API_KEY='ae6fe92c8537db8b7f4057781252c88e'
 export const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const countryLanguageMapping: Record<string, string[]> = {
